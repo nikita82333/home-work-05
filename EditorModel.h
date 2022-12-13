1a15e666f832004298f1177312adc124f66df5b2
Кузнецов Никита Вениаминович <kunv@kbl.nsk.su>
2022-12-13 11:47:28 +0700

    Revert "remove all files for review2"
    This reverts commit 398518dda8c1b092e41fbd4ca773a4fda4516a2f.

---
+++ EditorModel.h
@@ -0,0 +1,43 @@
+#ifndef EDITORMODEL_H
+#define EDITORMODEL_H
+
+#include <map>
+#include <utility>
+#include <memory>
+#include <fstream>
+#include <string>
+
+#include "Figure.h"
+
+using figure_storage = std::map<std::size_t, std::shared_ptr<Figure>>;
+
+/// <summary>
+/// Class EditorModel (MVC).
+/// </summary>
+class EditorModel {
+public:
+    void new_document();
+    void export_to_file();
+    void import_from_file();
+    template <typename T>
+    std::size_t add_figure(const T& figure);
+    void delete_figure(std::size_t id);
+    figure_storage get_figures();
+
+private:
+    std::size_t _id {0};
+    figure_storage _figures;
+    std::string _file_name {"save.bin"};
+};
+
+
+template <typename T>
+std::size_t EditorModel::add_figure(const T& figure) {
+    _figures.emplace(_id, std::make_shared<T>(figure));
+    std::size_t returning_id = _id;
+    ++_id;
+    return returning_id;
+}
+
+
+#endif //EDITORMODEL_H
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "EditorView.h"
+#include "EditorModel.h"
+#include "EditorController.h"
+
+int main() {
+
+    auto model = std::make_shared<EditorModel>();
+    auto controller = std::make_shared<EditorController>(model);
+    auto view = std::make_shared<EditorView>(model, controller);
+
+
+    view->new_document_click();
+    view->create_line_click({0,0}, {1,1});
+    view->create_rectangle_click({0,0}, {1,1});
+    view->create_triangle_click({0,1}, {0.5,1.5}, {1,1});
+    view->create_circle_click({0.5,0.5}, 0.5);
+
+    std::cout << "Figures is saved to a file." << std::endl;
+    view->export_to_file_click();
+    std::cout << "Delete figure with id = 2" << std::endl;
+    view->delete_figure_click(2);
+    std::cout << "Figures is loading from a file." << std::endl;
+    view->import_from_file_click();
+
+
+    return 0;
+}
